83fa57238ff6290c9e1fc3382cdf686ec9752767
Florian Dorau <fdorau@it-economics.de>
2021-02-26 20:27:19 +0100

    first commit

---
+++ src/main/kotlin/dev/dorau/springdgsdemo/SpringDgsDemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.dorau.springdgsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDgsDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDgsDemoApplication>(*args)
+}
---
+++ src/test/kotlin/dev/dorau/springdgsdemo/SpringDgsDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.dorau.springdgsdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDgsDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
